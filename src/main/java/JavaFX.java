f202e56237b151fe2e7526c153acc91a0e8af5ce
cmkhoa <cmkhoa2006@gmail.com>
2026-03-08 20:08:54 -0500

    Basic Data Output to terminal, adding svgs for images from https://github.com/Makin-Things/weather-icons/tree/main

--- src/main/java/JavaFX.java
+++ src/main/java/JavaFX.java
@@ -29,11 +29,25 @@ public class JavaFX extends Application {
 		weather = new TextField();
 		temperature.setText("Today's weather is: "+String.valueOf(forecast.get(0).temperature));
 		weather.setText(forecast.get(0).shortForecast);
+		// Iterate through all 14 periods (7 days of Day/Night
+		for (Period p : forecast) {
+			System.out.println("--- Period " + p.number + ": " + p.name + " ---");
+			System.out.println("Time: " + p.startTime + " to " + p.endTime);
+			System.out.println("Is Daytime: " + p.isDaytime);
+			System.out.println("Temp: " + p.temperature + " " + p.temperatureUnit);
+			System.out.println("Trend: " + p.temperatureTrend);
+			System.out.println("Precip: " + (p.probabilityOfPrecipitation != null ? p.probabilityOfPrecipitation.value : "0") + "%");
+			System.out.println("Wind: " + p.windSpeed + " from " + p.windDirection);
+			System.out.println("Short: " + p.shortForecast);
+			System.out.println("Detailed: " + p.detailedForecast);
+			System.out.println("Icon URL: " + p.icon);
+			System.out.println("-------------------------------------\n");
+		}
 		
 		
 		
 				
-		Scene scene = new Scene(new VBox(temperature,weather), 700,700);
+		Scene scene = new Scene(new VBox(temperature,weather), 540,960);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
